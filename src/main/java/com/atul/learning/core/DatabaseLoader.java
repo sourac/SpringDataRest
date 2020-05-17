f0d125c8c6b3a4dae1d892dcf481b7293d2880d1
Atul Sharma <atulsharma21772@gmail.com>
2020-05-17 14:20:24 +0530

    getting hands dirty.
    Signed-off-by: Atul Sharma <atulsharma21772@gmail.com>

--- src/main/java/com/atul/learning/core/DatabaseLoader.java
+++ src/main/java/com/atul/learning/core/DatabaseLoader.java
@@ -3,6 +3,10 @@
  */
 package com.atul.learning.core;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -33,7 +37,38 @@ public class DatabaseLoader implements ApplicationRunner {
 
 		Course course = new Course("Java Basics", "https://github.com/sourac");
 		course.addReview(new Review(3, "You're doing good"));
+		
+		String [] templates= {
+				"Up and Running with %s",
+				"%s basics",
+				"%s for beginners",
+				"%s for neckbeards",
+				"under the hood : %s"
+		};
+		
+		String [] buzzwords= {
+				"Spring Data Rest",
+				"Java 14",
+				"Scala",
+				"Groovy",
+				"Hibernate"
+		};
+		
+		
+		List<Course> bunchOfCourses=new ArrayList<Course>();
+		IntStream.range(0, 100).
+		forEach(i-> {
+			String template=templates[i % templates.length];
+			String buzzword=buzzwords[i % buzzwords.length];
+			String title=String.format(template, buzzwords);
+			Course c=new Course(title, "https://www.example.com");
+			c.addReview(new Review(i%5, String.format("More %s please", buzzword)));
+			bunchOfCourses.add(c);
+		});
+		
+		courses.saveAll(bunchOfCourses);
 		courses.save(course);
+		
 
 	}
 
